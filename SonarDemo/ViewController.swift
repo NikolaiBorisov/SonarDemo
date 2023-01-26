b56712002ed39a665845291a1d6af99b6ae444c8
Nikolai Borisov <Borisov_n_88@mail.ru>
2023-01-26 10:59:48 +0300

    Initial Commit

---
+++ SonarDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SonarDemo
+//
+//  Created by NIKOLAI BORISOV on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(view.backgroundColor)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
